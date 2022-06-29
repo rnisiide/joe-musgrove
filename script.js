51eabed65277f1fdc9f71c3595cead2b0653db35
Ricardo Nisiide <rnisiide@gmail.com>
2022-06-29 00:31:07 -0300

    added form for signing up, in a modal, that pops up at button clicks

---
+++ script.js
@@ -0,0 +1,27 @@
+function hideShow(val) {
+    if (val == "Y") {
+      document.getElementById("otherconfigarea").style.display = "block";
+    } else {  
+      document.getElementById("otherconfigarea").style.display = "none";
+    }
+  }
+
+
+function showCabin(val) {
+    if (val == "Y") {
+        document.getElementById("othercabinarea").style.display = "block";
+    } else {  
+        document.getElementById("othercabinarea").style.display = "none";
+    }
+}
+
+
+function aquisitionShow(val) {
+    if (val == 7) {
+        document.getElementById("aquisitionarea").style.display = "block";
+    } else {  
+        document.getElementById("aquisitionarea").style.display = "none";
+    }
+}
+  
+  
