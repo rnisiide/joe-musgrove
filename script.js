d67c163d8b2a94ed6292a5b5b3638524df6dfd82
Ricardo Nisiide <rnisiide@gmail.com>
2022-06-29 23:56:00 -0300

    changed caroussel and header to be responsive. picture album also responsive.

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ function aquisitionShow(val) {
 const scrollTrigger = 60;
 
 window.onscroll = function() {
-  // We add pageYOffset for compatibility with IE.
   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
     document.getElementsByTagName("header")[0].classList.add("visible-header");
   } else {
